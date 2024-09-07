0739778704027989bcdae3c87abf018c5369ac59
Lachlan McKinnie <89623046+Lachiemckbioinfo@users.noreply.github.com>
2024-09-07 20:07:51 +1000

    Add files via upload

--- code/analysis_dash.py
+++ code/analysis_dash.py
@@ -13,6 +13,11 @@ df['peopleIn'] = pd.to_numeric(df['peopleIn'], errors='coerce')
 df['peopleOut'] = pd.to_numeric(df['peopleOut'], errors='coerce')
 df['nonNegDigActivity'] = pd.to_numeric(df['nonNegDigActivity'], errors='coerce')
 df['relativeHumidity'] = pd.to_numeric(df['relativeHumidity'], errors='coerce')
+df['pademelon'] = pd.to_numeric(df['pademelon'], errors='coerce')
+df['red_necked_wallaby'] = pd.to_numeric(df['red_necked_wallaby'], errors='coerce')
+df['great_barred_frog'] = pd.to_numeric(df['great_barred_frog'], errors='coerce')
+df['yellow_throated_scrubwren'] = pd.to_numeric(df['yellow_throated_scrubwren'], errors='coerce')
+df['noisy_pitta'] = pd.to_numeric(df['noisy_pitta'], errors='coerce')
 
 # Group by date and calculate daily aggregates with max temperature
 daily_df = df.set_index('datetime').resample('D').agg({
@@ -21,7 +26,12 @@ daily_df = df.set_index('datetime').resample('D').agg({
     'airTemperature': 'max',
     'precipitation': 'sum',
     'peopleIn': 'sum',
-    'windSpeed': 'mean'
+    'windSpeed': 'mean',
+    'pademelon': 'sum',
+    'red_necked_wallaby': 'sum',
+    'great_barred_frog': 'sum',
+    'yellow_throated_scrubwren': 'sum',
+    'noisy_pitta': 'sum'
 }).reset_index()
 
 people_df = df.set_index('datetime').resample('D').agg({
@@ -29,7 +39,12 @@ people_df = df.set_index('datetime').resample('D').agg({
     'airTemperature': 'max',
     'precipitation': 'sum',
     'peopleIn': 'sum',
-    'windSpeed': 'mean'
+    'windSpeed': 'mean',
+    'pademelon': 'sum',
+    'red_necked_wallaby': 'sum',
+    'great_barred_frog': 'sum',
+    'yellow_throated_scrubwren': 'sum',
+    'noisy_pitta': 'sum'
 })
 people_df = people_df.dropna(subset=['peopleIn'])
 
@@ -38,63 +53,33 @@ dig_df = df.set_index('datetime').resample('D').agg({
     'airTemperature': 'max',
     'precipitation': 'sum',
     'nonNegDigActivity': 'sum',
-    'windSpeed': 'mean'
+    'windSpeed': 'mean',
+    'pademelon': 'sum',
+    'red_necked_wallaby': 'sum',
+    'great_barred_frog': 'sum',
+    'yellow_throated_scrubwren': 'sum',
+    'noisy_pitta': 'sum'
 })
 dig_df = dig_df.dropna(subset=['nonNegDigActivity'])
 
 # Calculate correlation matrix
 correlation_matrix = daily_df[['nonNegDigActivity', 'peopleIn', 'airTemperature', 
-                               'precipitation', 'relativeHumidity', 'windSpeed']].corr()
-corr_dig_df = dig_df[['nonNegDigActivity', 'airTemperature', 'precipitation', 'relativeHumidity', 'windSpeed']].corr()
-corr_people_df = people_df[['peopleIn', 'airTemperature', 'precipitation', 'relativeHumidity', 'windSpeed']].corr()
+                               'precipitation', 'relativeHumidity', 'windSpeed',
+                               'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren',
+                               'noisy_pitta']].corr()
+corr_dig_df = dig_df[['nonNegDigActivity', 'airTemperature', 'precipitation', 'relativeHumidity', 'windSpeed',
+                      'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren','noisy_pitta']].corr()
+corr_people_df = people_df[['peopleIn', 'airTemperature', 'precipitation', 'relativeHumidity', 'windSpeed',
+                            'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren','noisy_pitta']].corr()
 
 # Create a Dash application
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Set up graphs
-max_air_temp = px.line(daily_df, x='datetime', y='airTemperature', title='Max Air Temperature Per Day')
-
-total_visitor = px.line(daily_df, x='datetime', y='peopleIn', title='Total Visitor Count Per Day')
-
-total_dig = px.line(daily_df, x='datetime', y='nonNegDigActivity', title='Total Digital Activity Per Day')
-
-dig_v_airtemp = px.scatter(dig_df, x='nonNegDigActivity', y='airTemperature', title='Daily Digital Activity vs Max Air Temperature')
-
-
-dig_v_humid = px.scatter(dig_df, x='nonNegDigActivity', y='relativeHumidity', 
-                                                    title='Daily Digital Activity vs relative Humidity',
-                                                    labels={'nonNegDigActivity': 'Digital Activity', 'relativeHumidity': 'Relative Humidity' })
-
-dig_v_rain = px.scatter(dig_df, x='nonNegDigActivity', y='precipitation', 
-                                                    title='Daily Digital Activity vs Precipitation',
-                                                    labels={'nonNegDigActivity': 'Digital Activity', 'precipitation': 'Total Daily Precipitation'})
-
-dig_v_windspeed = px.scatter(dig_df, x='nonNegDigActivity', y='windSpeed',
-                             title='Daily Digital Activity vs Mean Wind Speed',
-                             labels = {'nonNegDigActivity': 'Digital Activity', 'windSpeed': 'Mean Wind Speed'})
-
-peopleIn_v_airtemp = px.scatter(people_df, x='peopleIn', y='airTemperature', 
-                                                        title='Daily People In vs Max Air Temperature',
-                                                        labels={"nonNegDigActivity": "Digital Activity", "airTemperature": "Maximum Air Temperature"})
-
-peopleIn_v_humid = px.scatter(people_df, x='peopleIn', y='relativeHumidity', 
-                                                        title='Daily Visitors In vs Relative Air Humidity',
-                                                        labels={'peopleIn': 'Total Visitors', 'relativeHumidity': 'Relative Humidity'})
-
-peopleIn_v_rain = px.scatter(people_df, x='peopleIn', y='precipitation', 
-                                                        title='Daily Visitors In vs Precipitation',
-                                                        labels={'peopleIn': 'Total Visitors', 'precipitation': 'Total Daily Precipitation'})
-peopleIn_v_windspeed = px.scatter(people_df, x='peopleIn', y='windSpeed',
-                             title='Daily Visitors vs Mean Wind Speed',
-                             labels = {'nonNegDigActivity': 'Digital Activity', 'windSpeed': 'Mean Wind Speed'})
-
+# Example animals: 'pademelon','red_necked_wallaby','great_barred_frog','yellow_throated_scrubwren','noisy_pitta'
 # App layout
 app.layout = dbc.Container([
     html.H1("Wildlife Park Data Visualization"),
-    html.H2("Correlation Matrix"),
-    dbc.Row([
-        dbc.Col(html.Pre(correlation_matrix.to_string()), md=12),
-    ]),
+    html.H1('General Statistics'),
     dbc.Row([
         dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='airTemperature', title='Max Air Temperature Per Day')), md=6),
         dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='peopleIn', title='Total Visitor Count Per Day')), md=6)
@@ -102,6 +87,7 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='nonNegDigActivity', title='Total Digital Activity Per Day')), md=12)
     ]),
+    html.H2('Digital Activity'),
     dbc.Row([
         dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='airTemperature', title='Daily Digital Activity vs Max Air Temperature')), md=6),
         dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='windSpeed', title='Daily Digital Activity vs Mean Wind Speed')), md=6)
@@ -118,9 +104,21 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='relativeHumidity', title='Daily People In vs Relative Humidity')), md=6),
         dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='precipitation', title='Daily People In vs Precipitation')), md=6)
+    ]),
+    html.H2('Animal Sightings'),
+    dbc.Row([
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='pademelon',
+                                            title='Daily Digital Activity vs Total Pademelon Sightings',
+                                            labels = {'nonNegDigitalActivity': 'Digital Activity','pademelon': 'Pademelon Sightings'})), md=12)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='red_necked_wallaby',
+                           title='Daily Digital Activity vs Total Red Necked Wallaby Sightings',
+                           labels = {'nonNegDigActivity': 'Digital Activity',
+                                     'red_necked_wallaby': 'Red Necked Wallaby Sightings'})), md=12)
     ])
 ], fluid=True)  # Ensure the container is fluid
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0', port=8050)
