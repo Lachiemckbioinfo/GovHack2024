aa9eae8c3bc666949940ab184d3d55d291ad8f27
Lachlan McKinnie <89623046+Lachiemckbioinfo@users.noreply.github.com>
2024-09-07 18:26:37 +1000

    Add files via upload

--- code/analysis_dash.py
+++ code/analysis_dash.py
@@ -20,14 +20,16 @@ daily_df = df.set_index('datetime').resample('D').agg({
     'relativeHumidity': 'mean',
     'airTemperature': 'max',
     'precipitation': 'sum',
-    'peopleIn': 'sum'
+    'peopleIn': 'sum',
+    'windSpeed': 'mean'
 }).reset_index()
 
 people_df = df.set_index('datetime').resample('D').agg({
     'relativeHumidity': 'mean',
     'airTemperature': 'max',
     'precipitation': 'sum',
-    'peopleIn': 'sum'
+    'peopleIn': 'sum',
+    'windSpeed': 'mean'
 })
 people_df = people_df.dropna(subset=['peopleIn'])
 
@@ -35,15 +37,16 @@ dig_df = df.set_index('datetime').resample('D').agg({
     'relativeHumidity': 'mean',
     'airTemperature': 'max',
     'precipitation': 'sum',
-    'nonNegDigActivity': 'sum'
+    'nonNegDigActivity': 'sum',
+    'windSpeed': 'mean'
 })
-dig_df = dig_df.dropna(subset='nonNegDigActivity')
+dig_df = dig_df.dropna(subset=['nonNegDigActivity'])
 
 # Calculate correlation matrix
 correlation_matrix = daily_df[['nonNegDigActivity', 'peopleIn', 'airTemperature', 
-                               'precipitation', 'relativeHumidity']].corr()
-corr_dig_df = dig_df[['nonNegDigActivity', 'airTemperature', 'precipitation', 'relativeHumidity']].corr()
-corr_people_df = people_df[['peopleIn', 'airTemperature', 'precipitation', 'relativeHumidity']].corr()
+                               'precipitation', 'relativeHumidity', 'windSpeed']].corr()
+corr_dig_df = dig_df[['nonNegDigActivity', 'airTemperature', 'precipitation', 'relativeHumidity', 'windSpeed']].corr()
+corr_people_df = people_df[['peopleIn', 'airTemperature', 'precipitation', 'relativeHumidity', 'windSpeed']].corr()
 
 # Create a Dash application
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -66,53 +69,57 @@ dig_v_rain = px.scatter(dig_df, x='nonNegDigActivity', y='precipitation',
                                                     title='Daily Digital Activity vs Precipitation',
                                                     labels={'nonNegDigActivity': 'Digital Activity', 'precipitation': 'Total Daily Precipitation'})
 
+dig_v_windspeed = px.scatter(dig_df, x='nonNegDigActivity', y='windSpeed',
+                             title='Daily Digital Activity vs Mean Wind Speed',
+                             labels = {'nonNegDigActivity': 'Digital Activity', 'windSpeed': 'Mean Wind Speed'})
+
 peopleIn_v_airtemp = px.scatter(people_df, x='peopleIn', y='airTemperature', 
                                                         title='Daily People In vs Max Air Temperature',
                                                         labels={"nonNegDigActivity": "Digital Activity", "airTemperature": "Maximum Air Temperature"})
 
 peopleIn_v_humid = px.scatter(people_df, x='peopleIn', y='relativeHumidity', 
-                                                        title='Daily People In vs Relative Air Humidity',
+                                                        title='Daily Visitors In vs Relative Air Humidity',
                                                         labels={'peopleIn': 'Total Visitors', 'relativeHumidity': 'Relative Humidity'})
 
 peopleIn_v_rain = px.scatter(people_df, x='peopleIn', y='precipitation', 
-                                                        title='Daily People In vs Precipitation',
+                                                        title='Daily Visitors In vs Precipitation',
                                                         labels={'peopleIn': 'Total Visitors', 'precipitation': 'Total Daily Precipitation'})
-
+peopleIn_v_windspeed = px.scatter(people_df, x='peopleIn', y='windSpeed',
+                             title='Daily Visitors vs Mean Wind Speed',
+                             labels = {'nonNegDigActivity': 'Digital Activity', 'windSpeed': 'Mean Wind Speed'})
 
 # App layout
-app.layout = html.Div([
+app.layout = dbc.Container([
     html.H1("Wildlife Park Data Visualization"),
     html.H2("Correlation Matrix"),
-    # Display the correlation matrices as preformatted text
     dbc.Row([
         dbc.Col(html.Pre(correlation_matrix.to_string()), md=12),
     ]),
     dbc.Row([
-        dbc.Col(html.Pre(corr_dig_df.to_string()), md=12),
+        dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='airTemperature', title='Max Air Temperature Per Day')), md=6),
+        dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='peopleIn', title='Total Visitor Count Per Day')), md=6)
     ]),
     dbc.Row([
-        dbc.Col(html.Pre(corr_people_df.to_string()), md=12),
+        dbc.Col(dcc.Graph(figure=px.line(daily_df, x='datetime', y='nonNegDigActivity', title='Total Digital Activity Per Day')), md=12)
     ]),
-    # General Stats row
     dbc.Row([
-        dbc.Col(dcc.Graph(id='total_visitor', figure=total_visitor), md=6),
-        dbc.Col(dcc.Graph(id='visitor-plot', figure=total_dig), md=6),
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='airTemperature', title='Daily Digital Activity vs Max Air Temperature')), md=6),
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='windSpeed', title='Daily Digital Activity vs Mean Wind Speed')), md=6)
     ]),
-    # Digital Activity Row
     dbc.Row([
-        dbc.Col(dcc.Graph(id='dig_v_airtemp', figure=dig_v_airtemp), md=4),
-        dbc.Col(dcc.Graph(id='dig_v_humid', figure=dig_v_humid), md=4),
-        dbc.Col(dcc.Graph(id='dig_v_rain', figure=dig_v_rain), md=4)
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='relativeHumidity', title='Daily Digital Activity vs Relative Humidity')), md=6),
+        dbc.Col(dcc.Graph(figure=px.scatter(dig_df, x='nonNegDigActivity', y='precipitation', title='Daily Digital Activity vs Precipitation')), md=6)
     ]),
-    # PeopleIn row
+    html.H2('People In'),
     dbc.Row([
-        dbc.Col(dcc.Graph(id='peopleIn_v_airtemp', figure=peopleIn_v_airtemp), md=4),
-        dbc.Col(dcc.Graph(id='peopleIn_v_humid', figure=peopleIn_v_humid), md=4),
-        dbc.Col(dcc.Graph(id='peopleIn_v_rain', figure=peopleIn_v_rain), md=4)
+        dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='airTemperature', title='Daily People In vs Max Air Temperature')), md=6),
+        dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='windSpeed', title='Daily People In vs Mean Wind Speed')), md=6)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='relativeHumidity', title='Daily People In vs Relative Humidity')), md=6),
+        dbc.Col(dcc.Graph(figure=px.scatter(people_df, x='peopleIn', y='precipitation', title='Daily People In vs Precipitation')), md=6)
     ])
-], fluid=True)
-
-
+], fluid=True)  # Ensure the container is fluid
 
 # Run the app
 if __name__ == '__main__':
